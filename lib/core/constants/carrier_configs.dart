8d8370c37e5861acab3a9a3c4709629e116c0ef4
Abdulrahman Abusnena <abdoabusnena39@gmail.com>
2026-05-20 13:36:21 +0200

    git commit

---
+++ lib/core/constants/carrier_configs.dart
@@ -0,0 +1,24 @@
+import 'package:odfinance/features/history/domain/models/voucher.dart';
+
+class CarrierConfigs {
+  static CarrierType identifyCode(String code) {
+    if (code.length == 14) return CarrierType.libyana;
+    if (code.length == 13) return CarrierType.almadar;
+    if (code.length == 15) return CarrierType.ltt;
+    return CarrierType.unkown;
+  }
+
+  static String generateCode(CarrierType carreier, String code) {
+    switch (carreier) {
+      case CarrierType.almadar:
+        return '*121*$code#';
+      case CarrierType.libyana:
+        return '*112*$code#';
+      case CarrierType.ltt:
+        return '*116*$code#';
+
+      case CarrierType.unkown:
+        return code;
+    }
+  }
+}
--- lib/features/history/domain/models/voucher.dart
+++ lib/features/history/domain/models/voucher.dart
@@ -1,1 +1,27 @@
+enum CarrierType { almadar, libyana, ltt, unkown }
 
+class Voucher {
+  final String id;
+  final String serialCode;
+  final CarrierType carrier;
+  final DateTime scannedAt;
+  final bool isUsed;
+
+  const Voucher({
+    required this.id,
+    required this.serialCode,
+    required this.carrier,
+    required this.scannedAt,
+    this.isUsed = false,
+  });
+
+  Voucher copyWith({bool? isUsed}) {
+    return Voucher(
+      id: id,
+      serialCode: serialCode,
+      carrier: carrier,
+      scannedAt: scannedAt,
+      isUsed: isUsed ?? this.isUsed,
+    );
+  }
+}
